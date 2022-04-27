bfbed289abea61855536f35ab76dc42a07513d2b
Max Wehner <maxwehnerwork@gmail.com>
2022-04-26 21:31:24 -0700

    finished prob 4

--- src/main/java/core_java_loops/Loops.java
+++ src/main/java/core_java_loops/Loops.java
@@ -6,6 +6,7 @@ public class Loops {
 		prob1();
 		prob2();
 		prob3();
+		prob4();
 	}
 
 	static void prob1() {
@@ -26,5 +27,16 @@ public class Loops {
 			System.out.println(i);
 			i++;
 		} while (i < 11);
+	}
+	
+	static void prob4() {
+		for(int i = 1; i < 101; i++) {
+			if(i > 25 && i < 75) {
+				continue;
+			}
+			if(i % 5 == 0) {
+				System.out.println(i);
+			}
+		}
 	}
 }
