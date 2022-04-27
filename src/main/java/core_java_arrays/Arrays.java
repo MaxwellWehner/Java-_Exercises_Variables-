54e4924dd0078fa2f834a323e4d5ec6daa77e8e1
Max Wehner <maxwehnerwork@gmail.com>
2022-04-27 08:11:49 -0700

    finished prob 2

--- src/main/java/core_java_arrays/Arrays.java
+++ src/main/java/core_java_arrays/Arrays.java
@@ -4,6 +4,7 @@ public class Arrays {
 
 	public static void main(String args[]) {
 		prob1();
+		prob2();
 	}
 
 	static void prob1() {
@@ -11,5 +12,10 @@ public class Arrays {
 		for(int i = 0; i < nums.length; i++) {
 			System.out.println(nums[i]);
 		}
+	}
+	
+	static void prob2() {
+		int nums[] = {13, 5, 7, 68, 2};
+		System.out.println(nums[(int)Math.floor(nums.length / 2)]);
 	}
 }
