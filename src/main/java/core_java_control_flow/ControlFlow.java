d6aa9ee116c874dee4716d5cfc0e866ca82570ac
Max Wehner <maxwehnerwork@gmail.com>
2022-04-27 09:55:03 -0700

    finished prob 5

--- src/main/java/core_java_control_flow/ControlFlow.java
+++ src/main/java/core_java_control_flow/ControlFlow.java
@@ -1,5 +1,7 @@
 package core_java_control_flow;
 
+import java.util.Scanner;
+
 public class ControlFlow {
 
 	public static void main(String[] args) {
@@ -14,10 +16,12 @@ public class ControlFlow {
 		// problem 3
 //		prob3(15);
 //		prob3(50);
-		
-		//problem 4
-		prob4(15);
-		prob4(5);
+
+		// problem 4
+//		prob4(15);
+//		prob4(5);
+
+		prob5();
 	}
 
 	static void prob1(int x) {
@@ -43,12 +47,31 @@ public class ControlFlow {
 			System.out.println("Greater than or equal to 20");
 		}
 	}
-	
+
 	static void prob4(int x) {
-		if(x < 10 || x > 20) {
+		if (x < 10 || x > 20) {
 			System.out.println("Out of range");
-		}else if(x >= 10 && x <= 20) {
+		} else if (x >= 10 && x <= 20) {
 			System.out.println("In range");
 		}
+	}
+
+	static void prob5() {
+		Scanner input = new Scanner(System.in);
+		int grade = input.nextInt();
+		if (grade >= 90 && grade <= 100) {
+			System.out.println("A");
+		} else if (grade >= 80 && grade <= 89) {
+			System.out.println("B");
+		} else if (grade >= 70 && grade <= 79) {
+			System.out.println("C");
+		} else if (grade >= 60 && grade <= 69) {
+			System.out.println("D");
+		} else if (grade >= 0 && grade < 60) {
+			System.out.println("F");
+		} else {
+			System.out.println("Score out of range");
+		}
+		input.close();
 	}
 }
