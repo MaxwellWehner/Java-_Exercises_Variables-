97bd866bad6a281c3734cf63295a76868ee161da
Max Wehner <maxwehnerwork@gmail.com>
2022-04-26 21:18:24 -0700

    finished prob 1

--- src/main/java/core_java_loops/Loops.java
+++ src/main/java/core_java_loops/Loops.java
@@ -3,7 +3,13 @@ package core_java_loops;
 public class Loops {
 
 	public static void main(String[] args) {
-		
-	}	
+		prob1();
+	}
+
+	static void prob1() {
+		for (int i = 1; i < 11; i++) {
+			System.out.println(i);
+		}
+	}
 
 }
