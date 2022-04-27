1c1f03658c20d40fae03ed532d848b95d702f6f5
Max Wehner <maxwehnerwork@gmail.com>
2022-04-27 09:41:21 -0700

    finished problem 3

--- src/main/java/core_java_control_flow/ControlFlow.java
+++ src/main/java/core_java_control_flow/ControlFlow.java
@@ -7,9 +7,13 @@ public class ControlFlow {
 //		prob1(7);
 //		prob1(15);
 
-		// problem 1
-		prob2(7);
-		prob2(15);
+		// problem 2
+//		prob2(7);
+//		prob2(15);
+
+		// problem 3
+		prob3(15);
+		prob3(50);
 	}
 
 	static void prob1(int x) {
@@ -23,6 +27,16 @@ public class ControlFlow {
 			System.out.println("Less than 10");
 		} else if (x > 10) {
 			System.out.println("Greater than 10");
+		}
+	}
+
+	static void prob3(int x) {
+		if (x < 10) {
+			System.out.println("Less than 10");
+		} else if (x > 10 && x < 20) {
+			System.out.println("Between 10 and 20");
+		} else if (x >= 20) {
+			System.out.println("Greater than or equal to 20");
 		}
 	}
 }
