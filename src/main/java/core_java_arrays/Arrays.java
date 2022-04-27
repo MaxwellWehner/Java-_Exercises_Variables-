a50ea546df09e0927b699fda835b9c6fdaaece79
Max Wehner <maxwehnerwork@gmail.com>
2022-04-27 08:05:34 -0700

    finished prob 1

--- src/main/java/core_java_arrays/Arrays.java
+++ src/main/java/core_java_arrays/Arrays.java
@@ -2,4 +2,14 @@ package core_java_arrays;
 
 public class Arrays {
 
+	public static void main(String args[]) {
+		prob1();
+	}
+
+	static void prob1() {
+		int nums[] = { 1, 2, 3 };
+		for(int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		}
+	}
 }
