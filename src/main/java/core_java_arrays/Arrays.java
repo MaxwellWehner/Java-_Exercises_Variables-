80400326482c907888fc87ba4131545d96606ef6
Max Wehner <maxwehnerwork@gmail.com>
2022-04-27 08:19:32 -0700

    finished prob 3

--- src/main/java/core_java_arrays/Arrays.java
+++ src/main/java/core_java_arrays/Arrays.java
@@ -3,19 +3,27 @@ package core_java_arrays;
 public class Arrays {
 
 	public static void main(String args[]) {
-		prob1();
-		prob2();
+//		prob1();
+//		prob2();
+		prob3();
 	}
 
 	static void prob1() {
 		int nums[] = { 1, 2, 3 };
-		for(int i = 0; i < nums.length; i++) {
+		for (int i = 0; i < nums.length; i++) {
 			System.out.println(nums[i]);
 		}
 	}
-	
+
 	static void prob2() {
-		int nums[] = {13, 5, 7, 68, 2};
-		System.out.println(nums[(int)Math.floor(nums.length / 2)]);
+		int nums[] = { 13, 5, 7, 68, 2 };
+		System.out.println(nums[(int) Math.floor(nums.length / 2)]);
+	}
+
+	static void prob3() {
+		String strs[] = { "red", "green", "blue", "yellow" };
+		System.out.println(strs.length);
+		String copy[] = strs.clone();
+		System.out.println(java.util.Arrays.toString(copy));
 	}
 }
