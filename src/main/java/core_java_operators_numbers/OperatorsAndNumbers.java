ad216fce50252605f5d4999ac1e76b8a4a5db295
Max Wehner <maxwehnerwork@gmail.com>
2022-04-26 22:09:50 -0700

    finsihed prob 1

--- src/main/java/core_java_operators_numbers/OperatorsAndNumbers.java
+++ src/main/java/core_java_operators_numbers/OperatorsAndNumbers.java
@@ -3,7 +3,15 @@ package core_java_operators_numbers;
 public class OperatorsAndNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// problem 1
+		// a) 1
+		// b) 1000
+		// c) 100001
+		// d) 1001110
+		// e) 1100010011
+		// f) 1000010011000011
+		
+		
 
 	}
 
