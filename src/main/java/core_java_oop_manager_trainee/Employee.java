42ab0c7fd478f318dcaf271426ed3102866e7586
Max Wehner <maxwehnerwork@gmail.com>
2022-04-27 17:16:53 -0700

    inital setup for manager employee hw

---
+++ src/main/java/core_java_oop_manager_trainee/Employee.java
@@ -0,0 +1,11 @@
+package core_java_oop_manager_trainee;
+
+public class Employee {
+	long employeeId;
+	String employeeName;
+	String employeeAddress;
+	long employeePhone;
+	double basicSalary;
+	double specialAllowance = 250.80;
+	double hra = 1000.50;
+}
